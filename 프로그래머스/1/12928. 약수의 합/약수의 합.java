0b96541c4779bd234650fef1b981d754eef7d0dd
kimju84 <71238153+kimju84@users.noreply.github.com>
2024-06-20 14:02:00 +0900

    [level 1] Title: 약수의 합, Time: 0.05 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,15 +1,8 @@
 class Solution {
     public int solution(int n) {
-        int answer =  n ;
-        if(n == 0)
-        {
-            return answer = 0;
-        }
-        else if (n == 1)
-        {
-            return answer = 1;
-        }
-        for (int i = 1; i < n; i++)
+        int answer =  0 ;
+      
+        for (int i = 1; i <= n; i++)
         {
             
             if (n % i == 0)
